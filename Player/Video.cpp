0eb1e4abcc8e1ad9d03b21d638924fc0c2a71eeb
cuiruiqi <crqhit@163.com>
2018-10-17 18:21:51 +0800

    增加解封装，解码线程

--- Player/Video.cpp
+++ Player/Video.cpp
@@ -11,8 +11,22 @@ Video::~Video()
 {
 }
 
-void Video::VideoInit()
+void Video::VideoPlay()
 {
+	Parse parse_video;
+	parse_video.ParseStream();
+	m_decode_th = thread(&Parse::DecodeThread, &parse_video);
+	m_unpacket_th = thread(&Parse::UnpacketThread, &parse_video);
+	parse_video.DisplayThread();
+	m_unpacket_th.join();
+	m_decode_th.join();
+	//this_thread::sleep_for(5s);
+}
+
+void Parse::ParseStream()
+{
+	m_url = (char*)"D:/work/Resource/oceans.mp4";
+	avformat_network_init();
 	int err;
 	err = avformat_open_input(&m_fmt_ctx, m_url, nullptr, nullptr);
 	if (err < 0)
@@ -22,15 +36,17 @@ void Video::VideoInit()
 	if (err < 0)
 		return;
 
+	av_dump_format(m_fmt_ctx, 0, m_url, 0);
+
 	AVCodec *dec;
 	int v_index = av_find_best_stream(m_fmt_ctx, AVMEDIA_TYPE_VIDEO, -1, -1, &dec, 0);
-	m_vctx = avcodec_alloc_context3(dec);
+	m_avcodec_ctx = avcodec_alloc_context3(dec);
 
-	avcodec_parameters_to_context(m_vctx, m_fmt_ctx->streams[v_index]->codecpar);
+	avcodec_parameters_to_context(m_avcodec_ctx, m_fmt_ctx->streams[v_index]->codecpar);
 
-	m_vctx->pkt_timebase = m_fmt_ctx->streams[v_index]->time_base;
+	m_avcodec_ctx->pkt_timebase = m_fmt_ctx->streams[v_index]->time_base;
 
-	if ((err = avcodec_open2(m_vctx, dec, nullptr) < 0))
+	if ((err = avcodec_open2(m_avcodec_ctx, dec, nullptr) < 0))
 	{
 		printf_s("Cannot open video decoder.\n");
 		return;
@@ -39,18 +55,23 @@ void Video::VideoInit()
 	//
 	AVStream *v_stream = m_fmt_ctx->streams[v_index];
 
+	int srcW = m_avcodec_ctx->width;
+	int srcH = m_avcodec_ctx->height;
+
+	m_pkt = av_packet_alloc();
+	m_frame = av_frame_alloc();
+	m_disframe = av_frame_alloc();
+	out_buffer = (unsigned char *)av_malloc(av_image_get_buffer_size(AV_PIX_FMT_RGB32, m_avcodec_ctx->width, m_avcodec_ctx->height, 1));
+	av_image_fill_arrays(m_disframe->data, m_disframe->linesize, out_buffer,
+		AV_PIX_FMT_RGB32, m_avcodec_ctx->width, m_avcodec_ctx->height, 1);
+	m_sws_ctx = sws_getContext(srcW, srcH, m_avcodec_ctx->pix_fmt, 
+		srcW, srcH, AV_PIX_FMT_RGB32, SWS_BICUBIC, nullptr, nullptr, nullptr);
+
+	
+	
 }
 
-void Video::VideoPlay()
-{
-	VideoInit();
-	Parse parse(m_vctx);
-
-	m_decode_th = thread(&Parse::ParseThread, &parse);
-	m_parse_th = thread(&Parse::DecodeThread, &parse);
-}
-
-void Parse::ParseThread()
+void Parse::UnpacketThread()
 {
 	for (;;)
 	{
@@ -62,11 +83,15 @@ void Parse::ParseThread()
 			continue;
 		}
 
-		lock_guard<mutex> guard(m_mutex);
-		m_pkts.push(m_pkt);
-		av_packet_unref(m_pkt);
-		m_bpkt_pushed = true;
-		m_cond_var.notify_one();
+		if (m_pkt->stream_index == AVMEDIA_TYPE_VIDEO)
+		{
+			lock_guard<mutex> guard(m_mutex);
+			AVPacket *pkt = av_packet_clone(m_pkt);
+			m_pkts.push(pkt);
+			//av_packet_unref(m_pkt);
+			m_bpkt_pushed = true;
+			m_cond_var.notify_one();
+		}
 	}
 }
 
@@ -79,7 +104,9 @@ void Parse::DecodeThread()
 			break;
 		unique_lock<std::mutex> mlock(m_mutex);
 		m_cond_var.wait(mlock, bind(&Parse::IsPacketReady, this));
-		AVPacket *pkt = m_pkts.front();
+		AVPacket *pkt = av_packet_alloc();
+		av_packet_ref(pkt, m_pkts.front());
+		m_pkts.pop();
 		m_bpkt_pushed = false;
 		
 		ret = avcodec_send_packet(m_avcodec_ctx, pkt);
@@ -89,12 +116,64 @@ void Parse::DecodeThread()
 		if (ret < 0)
 			continue;
 
-		m_frames.push(m_frame);
-		av_frame_unref(m_frame);
+		AVFrame *frame = av_frame_clone(m_frame);
+		m_frames.push(frame);
+		//av_frame_unref(m_frame);
 	}
 }
 
 bool Parse::IsPacketReady()
 {
 	return m_bpkt_pushed;
+}
+
+void Parse::DisplayThread()
+{
+	for (;;)
+	{
+		if (m_control)
+		{
+			switch (m_order)
+			{
+			case PLAY:
+				break;
+			default:
+				break;
+			}
+		}
+		else
+		{
+			av_usleep((int64_t)30 * 1000);
+			lock_guard<mutex> guard(m_mutex_frame);
+			if (m_frames.size() <= 0)
+			{
+				continue;
+			}
+			av_frame_ref(m_frame, m_frames.front());
+			//6m_disframe = m_frames.front();
+			m_frames.pop();
+
+			sws_scale(m_sws_ctx, (const uint8_t * const *)m_frame->data, m_frame->linesize,
+				0, m_disframe->height, m_disframe->data, m_disframe->linesize);
+
+			QImage tmpImg((uchar *)out_buffer, m_avcodec_ctx->width, m_avcodec_ctx->height, QImage::Format_RGB32);
+			int err = tmpImg.save("test.png");
+		}
+	}
+	/*
+	for(;;)
+	{
+		if(controls)
+		{
+			video control;
+		}
+		else
+		{
+			if(delay > 0)
+				sleep(delay);
+			reset delay;
+			display frame and caculate delay;
+		}
+	}
+	*/
 }
--- Player/Video.h
+++ Player/Video.h
@@ -9,38 +9,67 @@
 extern "C"
 {
 #include <libavformat/avformat.h>
+#include <libavutil/time.h>
+#include <libavutil/imgutils.h>
+#include <libswscale/swscale.h>
 }
 
 #include <queue>
 #include <thread>
 #include <mutex>
+#include <QtGui/QImage>
+#include <QtCore/QString>
 using namespace std;
 
+enum CONTROL
+{
+	PLAY,
+	PAUSE,
+	STOP,
+	SAVEPIC,
+	FASTFORWARD,
+	FASTBACKWARD
+};
 class Parse
 {
 public:
-	Parse(AVCodecContext *codec_ctx) 
-	{
-		m_avcodec_ctx = codec_ctx;
-	}
+	Parse() {}
 	~Parse() {}
 
-	void ParseThread();		//sub thread: package parse. to m_pkts
+	void ParseStream();
+
+	void UnpacketThread();	//sub thread: package parse. to m_pkts
 	void DecodeThread();	//sub thread: decoding pkt to frame. from m_pkts
 	bool IsPacketReady();	//to make sure whether packet in queue.
+	void DisplayThread();	//Render frames.	Maybe main thread, not need to create new sub thread.
 
 private:
+	void pgm_save(unsigned char *buf, int wrap, int xsize, int ysize,
+		char *filename);
+
+private:
+	//thread m_parse_th;		//for Parse thread.
+	//thread m_decode_th;		//for Decode thread.
+
 	mutex m_mutex;
+	mutex m_mutex_frame;
 	condition_variable m_cond_var;
 	bool m_bpkt_pushed;
 	bool m_bquit;
+	bool m_control;
+	CONTROL m_order;
 
-	AVFormatContext *m_fmt_ctx;
-	AVPacket *m_pkt;
-	AVFrame *m_frame;
-	AVCodecContext *m_avcodec_ctx;
-	queue<AVPacket *> m_pkts;	//packets.
-	queue<AVFrame *> m_frames;	//frames
+	char *m_url;					//url for play.
+	AVFormatContext *m_fmt_ctx;		//
+	AVDictionary *m_fmt_opts;		//
+	AVPacket *m_pkt;				//
+	AVFrame *m_frame;				//
+	AVFrame *m_disframe;
+	AVCodecContext *m_avcodec_ctx;	//
+	queue<AVPacket *> m_pkts;		//packets.
+	queue<AVFrame *> m_frames;		//frames
+	SwsContext *m_sws_ctx;			//scale context
+	unsigned char *out_buffer;
 };
 
 
@@ -50,24 +79,13 @@ public:
 	Video();
 	~Video();
 
-	void VideoInit();
 	void VideoPlay();		//main thread: play video.
 
 private:
-	void VideoDisplay();	//Render frames.
-	void VideoControl();	//control video.
+	//void VideoControl();	//control video.
 
 private:
-	
-	//queue<AVFrame> m_frames;//frames to render.
-	
-	thread m_parse_th;		//for Parse thread.
+	thread m_unpacket_th;		//for Parse thread.
 	thread m_decode_th;		//for Decode thread.
-
-	char *m_url;			//url for play.
-	AVFormatContext *m_fmt_ctx;	//
-	AVDictionary *m_fmt_opts;	//
-
-	AVCodecContext *m_vctx;	//
 };
 
